601379a0ac910ad0b8f6fb08492b93c46c3362d9
atasinonagagutu <simuma712@gmail.com>
2021-03-16 17:28:11 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,13 @@ class Item < ApplicationRecord
   belongs_to :Prefecture
   belongs_to :delivery_day
 
+  with_options presence:true do
+  validates :name, :description, :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+  validates :category_id, :status_id, :delivery_fee_id,
+            :prefecture_id, :delivery_day_id
+  end
+
 end
