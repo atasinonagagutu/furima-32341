5196333e7564a5e3b8996c6b210e9cadc0ec1c4c
atasinonagagutu <simuma712@gmail.com>
2021-03-18 16:40:12 +0900

    販売利益計算と単体テストの実施

---
+++ app/javascript/count.js
@@ -0,0 +1,12 @@
+function count (){
+  const itemPrice = document.getElementById('item-price')
+  itemPrice.addEventListener("keyup", () => {
+    const taxPrice = itemPrice.value * 0.1
+    const addTaxPrice = document.getElementById('add-tax-price')
+      addTaxPrice.innerHTML = taxPrice
+    const profit = document.getElementById('profit')
+      profit.innerHTML = itemPrice.value - taxPrice
+  });
+}
+
+window.addEventListener('load', count);
