230a53368f22d1e5eb2985008137149a967fa020
atasinonagagutu <simuma712@gmail.com>
2021-03-29 11:04:06 +0900

    配送情報保存機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,23 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render action: :index
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address_line, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,16 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :address_line, :building, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :address_line, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
     validates :city, :address_line
   end
-  validates :prefecture, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
     purchase_user = PurchaseUser.create(user_id: current_user.id, item_id: params[:item_id])
-    Destination.create(postal_code: postal_code, prefecture: prefecture, city: city, address_line: address_line, building: building, purchase_user_id = purchase_user.id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture, city: city, address_line: address_line, building: building, purchase_user_id: purchase_user)
+  end
 end
