0773b2f0ef7272e134696ca0a01f43a55239849f
atasinonagagutu <simuma712@gmail.com>
2021-03-30 15:26:46 +0900

    クレジット機能トークン化を行うファイルの作成

---
+++ app/javascript/card.js
@@ -0,0 +1,7 @@
+const pay = () => {
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("フォーム送信時にイベント発火")
+  });
+}
